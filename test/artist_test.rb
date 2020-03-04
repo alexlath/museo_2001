eb97dbf8ab628eb093234316d7a60d4eec3b690e
Alex Latham <alexlthm@gmail.com>
2020-03-04 09:14:37 -0700

    Add method and test for age at death

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -23,4 +23,8 @@ class ArtistTest < Minitest::Test
     assert_equal "1984", @artist.died
     assert_equal "United States", @artist.country
   end
+
+  def test_it_can_calculate_age_at_death
+    assert_equal 82, @artist.age_at_death
+  end
 end
