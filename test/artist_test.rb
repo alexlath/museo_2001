0e998a7013bc575ade0ab548d3ea9edb39864705
Alex Latham <alexlthm@gmail.com>
2020-03-04 09:11:18 -0700

    Add artist exists and attributes tests

---
+++ test/artist_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'artist'
+
+class ArtistTest < Minitest::Test
+  def setup
+    attributes = {id: "2",
+                  name: "Ansel Adams",
+                  born: "1902",
+                  died: "1984",
+                  country: "United States"}
+    @artist = Artist.new(attributes)
+  end
+
+  def test_it_can_exist
+    assert_instance_of Artist, @artist
+  end
+
+  def test_it_has_attributes
+    assert_equal "2", @artist.id
+    assert_equal "Ansel Adams", @artist.name
+    assert_equal "1902", @artist.born
+    assert_equal "1984", @artist.died
+    assert_equal "United", @artist.country
+  end
+end
